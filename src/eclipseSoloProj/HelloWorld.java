553c2ea1aee4189589eb856016a653f07ddce504
CharlesXMurphy <cmurph52@gmail.com>
2025-08-08 00:18:41 -0400

    Did some Nifty things

--- src/eclipseSoloProj/HelloWorld.java
+++ src/eclipseSoloProj/HelloWorld.java
@@ -1,5 +1,10 @@
 package eclipseSoloProj;
 
 public class HelloWorld {
-
+	
+	public static void main (String[] args) {
+		System.out.println("This is my first comment");
+		System.out.println("This is my second comment");
+		
+	}
 }
